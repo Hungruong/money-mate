f874b4a8808e45c67580616075d54ae004fdf2f8
Alice Dang <dthuhang214@gmail.com>
2025-02-11 00:10:01 -0500

    Update FE Authorization Draft2

--- frontend/app/screens/Auth/SignIn/index.tsx
+++ frontend/app/screens/Auth/SignIn/index.tsx
@@ -4,11 +4,6 @@ import { useNavigation } from "@react-navigation/native";
 import { AuthNavigationProp } from "../../../types/navigation";
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
 
 import { TextInput } from 'react-native';
 
@@ -17,6 +12,7 @@ import {Alert, Linking} from 'react-native';
 import {ImageBackground} from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { StatusBar } from 'react-native';
+
 // Importing the class from the location of the file
 
 
@@ -29,55 +25,44 @@ export default function SignInScreen({ setIsAuthenticated }: { setIsAuthenticate
   const navigation = useNavigation<AuthNavigationProp>();
 
   return (
-    //<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      //<Text style={{ fontSize: 24, fontWeight: "bold" }}>Sign In</Text>
-      //<Button title="Go to Sign Up" onPress={() => navigation.navigate("SignUp")} />
-      //<Button title="Sign In" onPress={() => setIsAuthenticated(true)} />
-    //</View>
+
     <View style={styles.root}>
     <StatusBar backgroundColor="black" barStyle="dark-content" />
 
     <ImageBackground style={styles.background} source={require('@/assets/images/background5.png')}>
 
 
-      <Text style={{textAlign:'center', fontSize: 30}}>Sign In</Text>
-      <Text style={{marginTop:10,textAlign:'center', fontSize: 20, color:'grey'}}>Hi! Welcome back </Text>
+      <Text style={styles.title}>Sign In</Text>
+      <Text style={styles.subtitle}>Hi! Welcome back </Text>
     
       
       
-      <Text style={{marginTop:50,fontSize:20,left:10}}>Email</Text>
-      <TextInput style={styles.inputTextbox} placeholder='  moneymate@gmail.com'></TextInput>
-      <Text style={{marginTop:10,fontSize:20,left:10}}>Password</Text>
-      <TextInput style={styles.inputTextbox} placeholder='  ***********'></TextInput>
-      <Text style={{color: '#a91337b', textAlign:'right',marginTop:10,right:10,textDecorationLine:'underline'}} onPress={() => Linking.openURL('THIS INCLUDE LINK')}> Forgot Password </Text>
-
-
-
+      <Text style={styles.text1}>Email</Text>
+      <TextInput style={styles.inputTextbox} placeholder='  Please enter your email'></TextInput>
+      <Text style={styles.text1}>Password</Text>
+      <TextInput style={styles.inputTextbox} placeholder='  Please enter your password'></TextInput>
+      <Text style={styles.forgotPasswordText} onPress={() => Linking.openURL('THIS INCLUDE LINK')}> Forgot Password </Text>
 
       
-      <View style={{ alignItems: 'center', marginTop: 20 }}>
+
+
+      <View style={styles.button}>
         <TouchableOpacity 
-          onPress={() => setIsAuthenticated(true)} 
-          style={{ 
-            backgroundColor: '#219ebc', 
-            paddingVertical: 10, 
-            paddingHorizontal: 20, 
-            borderRadius: 25, // Makes it rounded
-          }}
-        >
-          <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>SIGN IN</Text>
+          onPress={() => console.log("Handle Sign Up")} 
+          style={styles.buttonContent}>
+          <Text style={styles.buttonText}>SIGN IN</Text>
         </TouchableOpacity>
       </View>
+
       <Text style={styles.text}>Or sign in with</Text>
-      <Image style={{width:50,height:50,borderRadius:1000,alignSelf:'center'}}
-          source={require('@/assets/images/google_logo.jpg')}
-      />
+      <Image style={styles.logo}
+          source={require('@/assets/images/google_logo.jpg')}/>
 
 
-      <View style={{ flexDirection: 'row', alignItems: 'center', alignSelf:'center', marginTop: 30 }}>
-        <Text style={{fontSize:20, marginTop:0}}>Don't have an account ?</Text>
+      <View style={styles.container2}>
+        <Text style={styles.button2}>Don't have an account ?</Text>
         
-        <Button title="Sign Up" onPress={() => navigation.navigate("SignUp")} color="#219ebc"  />
+        <Button title="Sign Up" onPress={() => navigation.navigate("SignUp")} color="#91337b" />
       </View>
     
     </ImageBackground>
@@ -87,20 +72,17 @@ export default function SignInScreen({ setIsAuthenticated }: { setIsAuthenticate
 
 
 const styles=StyleSheet.create({
-  root: {flex:1, fontFamily:'GeneralSans-Regular'},
+  root: {
+    flex:1, 
+    fontFamily:'GeneralSans-Regular',
+    alignContent:'center',
+},
   container:{
     flex:2,textAlign:'center',backgroundColor: 'white',
   },
   image:{opacity:2},
   text:{
-    textAlign:'center',fontSize:20, marginTop:30
-  },
-  lefttext:{
-    textAlign:'left',fontSize:20,
-  },
-  content: {
-    flex: 1,
-    paddingTop: StatusBar.currentHeight, // Ensures content does not overlap with status bar
+    textAlign:'center',fontSize:20
   },
 
 
@@ -112,28 +94,25 @@ const styles=StyleSheet.create({
   inputTextbox:{
     height: 40,
     width:370,
-    bottom: 0,
-    left:0,
     margin:10,
     borderWidth:1,
     color: 'grey',
     borderRadius:10,
 
   },
-  marginAlgin:{
-    marginTop:10,
-  },
+
   button: {
     marginTop:30,
     backgroundColor: '#91337b',
     padding: 10,
-    borderRadius: 5,
-    width:100,
+    borderRadius: 10,
+    width:370,
     alignSelf:'center',
-    
+    marginBottom:20,
   },
-  buttonText: {
-    color: 'black',
+  
+  buttonContent: {
+    color: '#91337b',
     fontSize: 16,
     alignSelf:'center'
   },
@@ -147,5 +126,56 @@ const styles=StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  title:{
+    textAlign:'center', 
+    fontSize: 30
+  },
+  subtitle:{
+    marginTop:10,
+    textAlign:'center', 
+    fontSize: 20, 
+    color:'grey',
+    marginBottom:30
+  },
+  text1:{
+    marginTop:20,
+    fontSize:20,
+    left:10
+  },
+  forgotPasswordText:{
+    color: '#91337b', 
+    textAlign:'right',
+    marginTop:10,
+    right:10,
+    fontSize:15,
+    textDecorationLine:'underline'
+  },
+  buttonCover:{
+    alignItems: 'center', 
+    marginTop: 20,
+    
+  },
+  signInButton:{
+    backgroundColor: '#219ebc', 
+    paddingVertical: 10, 
+    paddingHorizontal: 20, 
+    borderRadius: 25,
+  },
+  buttonContent1:{
+    color: 'white', fontSize: 20, fontWeight: 'bold'
+  },
+  logo:{
+    width:50,
+    height:50,
+    borderRadius:1000,
+    alignSelf:'center',
+    marginTop:15,
+  },
+  container2:{ flexDirection: 'row', alignItems: 'center', alignSelf:'center', marginTop: 30 },
+  button2:{fontSize:20},
+
+
+  buttonText:{ color: 'white', fontSize: 16, fontWeight: 'bold' },
+
  
 })
--- frontend/app/screens/Auth/SignUp/index.tsx
+++ frontend/app/screens/Auth/SignUp/index.tsx
@@ -1,77 +1,43 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-import {View} from "react-native";
-import { TextInput } from 'react-native';
-import { Button } from 'react-native';
-import React, {useCallback} from 'react';
-import {Alert, Linking} from 'react-native';
-import {ImageBackground, Text} from 'react-native';
-import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
-import { StatusBar } from 'react-native';
-// Importing the class from the location of the file
-
+import React, { useCallback } from 'react';
+import { Image, StyleSheet, View, TextInput, Button, ImageBackground, Text, StatusBar, TouchableOpacity} from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 import { AuthNavigationProp } from "../../../types/navigation";
 
-// Creating an object of the class which is imported
-
-import { TouchableOpacity } from 'react-native';
         
 export default function SignUpScreen() {
   const navigation = useNavigation<AuthNavigationProp>();
   return (
-    
       <View style={styles.root}>
         <StatusBar backgroundColor="black" barStyle="dark-content" />
-
         <ImageBackground style={styles.background} source={require('@/assets/images/background5.png')}>
 
-  
-          <Text style={{marginTop:10,textAlign:'center', fontSize: 30}}>Sign Up</Text>
-          <Text style={{marginTop:10,textAlign:'center', fontSize: 20, color:'grey'}}>Let's create your own account</Text>
+          <Text style={styles.title}>Sign Up</Text>
+          <Text style={styles.subText}>Let's create your own account</Text>
         
-          
-          
-          <Text style={{marginTop:10,fontSize:20,left:10}}>First name</Text>
-          <TextInput style={styles.inputTextbox} placeholder=' Alice'></TextInput>
-          <Text style={{marginTop:5,fontSize:20,left:10}}>Last name</Text>
-          <TextInput style={styles.inputTextbox} placeholder=' Dang'></TextInput>
-          <Text style={{marginTop:5,fontSize:20,left:10}}>Email</Text>
-          <TextInput style={styles.inputTextbox} placeholder='  moneymate@gmail.com'></TextInput>
-          <Text style={{marginTop:5,fontSize:20,left:10}}>Password</Text>
-          <TextInput style={styles.inputTextbox} placeholder='  ***********'></TextInput>
+          <Text style={styles.text1}>First name</Text>
+          <TextInput style={styles.inputTextbox} placeholder=' Please enter your first name'></TextInput>
+          <Text style={styles.text1}>Last name</Text>
+          <TextInput style={styles.inputTextbox} placeholder=' Please enter your last name'></TextInput>
+          <Text style={styles.text1}>Email</Text>
+          <TextInput style={styles.inputTextbox} placeholder='  Please enter your email'></TextInput>
+          <Text style={styles.text1}>Password</Text>
+          <TextInput style={styles.inputTextbox} placeholder='  Please enter your password'></TextInput>
 
-        
-          
-          <View style={{ alignItems: 'center', marginTop: 20 }}>
+          <View style={styles.button}>
             <TouchableOpacity 
               onPress={() => console.log("Handle Sign Up")} 
-              style={{ 
-                backgroundColor: '#219ebc', 
-                paddingVertical: 10, 
-                paddingHorizontal: 20, 
-                borderRadius: 25, // Makes it rounded
-              }}
-            >
-              <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>SIGN UP</Text>
+              style={styles.buttonContent}>
+              <Text style={styles.buttonText}>SIGN UP</Text>
             </TouchableOpacity>
           </View>
-          <Text style={styles.text}>Or sign up with</Text>
-          <Image style={{ marginTop:5,width:50,height:50,borderRadius:100,alignSelf:'center'}}
-              source={require('@/assets/images/google_logo.jpg')}
-          />
 
-    
-          
-          <View style={{ flexDirection: 'row', alignItems: 'center', alignSelf:'center', marginTop: 30 }}>
-            <Text style={{fontSize:20, marginTop:0}}>Already had an account ?</Text>
-            
-            <Button title="Sign In" onPress={() => navigation.navigate("SignIn")} color="#219ebc"  />
+          <Text style={styles.text}>Or sign up with</Text>
+          <Image style={styles.button1Text2}
+              source={require('@/assets/images/google_logo.jpg')}/>
+   
+          <View style={styles.signInNavigateBox}>
+            <Text style={styles.navigationBoxText}>Already had an account ?</Text>
+            <Button title="Sign In" onPress={() => navigation.navigate("SignIn")} color="#91337b"  />
           </View>
         
         </ImageBackground>
@@ -83,28 +49,33 @@ export default function SignUpScreen() {
 }
 
 const styles=StyleSheet.create({
-  root: {flex:1, fontFamily:'GeneralSans-Regular'},
-  container:{
-    flex:2,textAlign:'center',backgroundColor: 'white',
-  },
-  image:{opacity:2},
-  text:{
-    textAlign:'center',fontSize:20, marginTop:10
-  },
-  lefttext:{
-    textAlign:'left',fontSize:20,
-  },
-  content: {
-    flex: 1,
-    paddingTop: StatusBar.currentHeight, // Ensures content does not overlap with status bar
+  root: {
+    flex:1, 
+    fontFamily:'GeneralSans-Regular'
   },
 
+  text:{
+    textAlign:'center',
+    fontSize:20, 
+    marginTop:10
+  },
 
   header:{
     flex:1,
-    
 
   },
+  
+  buttonText:{ 
+    color: 'white', 
+    fontSize: 16, 
+    fontWeight: 'bold' },
+
+  buttonContent: {
+    color: '#91337b',
+    fontSize: 16,
+    alignSelf:'center'
+  },
+
   inputTextbox:{
     height: 40,
     width:370,
@@ -114,29 +85,26 @@ const styles=StyleSheet.create({
     borderWidth:1,
     color: 'grey',
     borderRadius:10,
-
   },
+
   marginAlgin:{
     marginTop:10,
   },
+
   button: {
-    marginTop:20,
+    marginTop:30,
     backgroundColor: '#91337b',
     padding: 10,
-    borderRadius: 5,
-    width:100,
+    borderRadius: 10,
+    width:370,
     alignSelf:'center',
-    
+    marginBottom:20,
   },
-  buttonText: {
-    color: 'black',
-    fontSize: 16,
-    alignSelf:'center',
-    marginTop:5,
-  },
+
   underlinedText:{
     textDecorationLine:'underline',
   },
+
   background: {
     flex:1,
     justifyContent: 'center', // Centers content vertically
@@ -144,5 +112,57 @@ const styles=StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+
+  title:{
+    marginTop:10,
+    textAlign:'center', 
+    fontSize: 30
+  },
+
+  subText:{
+    marginTop:10,
+    textAlign:'center', 
+    fontSize: 20, 
+    color:'grey',
+    marginBottom:30,
+  },
+
+  text1:{
+    marginTop:10,
+    fontSize:20,
+    left:10
+  },
+
+  button1:{
+    alignItems: 'center', 
+    marginTop: 20
+  },
+
+  button1Text:{ 
+    backgroundColor: '#219ebc', 
+    paddingVertical: 10, 
+    paddingHorizontal: 20, 
+    borderRadius: 25, // Makes it rounded
+  },
+
+  button1Text2:{ 
+    marginTop:5,
+    width:50,
+    height:50,
+    borderRadius:100,
+    alignSelf:'center'
+  },
+
+  signInNavigateBox:{ 
+    flexDirection: 'row', 
+    alignItems: 'center',
+    alignSelf:'center', 
+    marginTop: 30 
+  },
+
+  navigationBoxText:{
+    fontSize:20, 
+    marginTop:0
+  }
  
 })
