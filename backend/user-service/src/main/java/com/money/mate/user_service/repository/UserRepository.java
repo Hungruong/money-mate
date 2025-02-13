17e38c0595c9380eee2bbb7afd1b5c4762514b6f
Uyen Tran <ednauynuyn@gmail.com>
2025-02-12 20:47:38 -0600

    git

--- backend/user-service/src/main/java/com/money/mate/user_service/repository/UserRepository.java
+++ backend/user-service/src/main/java/com/money/mate/user_service/repository/UserRepository.java
@@ -10,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByFirebaseUid(String firebaseUid);
 
+    Optional<User> findByPhoneNumber(String phoneNumber);
+
 }
