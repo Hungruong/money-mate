41abfd60f61da1cdef11b60e25d5984df0fb5314
Alice Dang <dthuhang214@gmail.com>
2025-04-13 12:36:12 -0400

    Commit

--- frontend/app/navigation/BottomTabNavigator.tsx
+++ frontend/app/navigation/BottomTabNavigator.tsx
@@ -1,10 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import HomeScreen from "../screens/home";
-import TradingScreen from "../screens/trading";
+
 import GroupSavingScreen from "../screens/GroupSaving";
 import BillSplitScreen from "../screens/BillSplit";
 import AppNavigator from "./UserProfileNavigator";
+import GroupSavingNavigator from "./GroupSavingNavigator";
 import TradingNavigator from "./TradingNavigator";
 
 const Tab = createBottomTabNavigator();
@@ -29,13 +30,8 @@ export default function BottomTabNavigator() {
       })}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
-<<<<<<< HEAD
-      <Tab.Screen name="Trading" component={TradingScreen} />
-      <Tab.Screen name="Group Saving" component={GroupSavingNavigator} />
-=======
       <Tab.Screen name="Trading" component={TradingNavigator} />
-      <Tab.Screen name="Group Saving" component={GroupSavingScreen} />
->>>>>>> origin/master
+      <Tab.Screen name="Group Saving" component={GroupSavingNavigator} />
       <Tab.Screen name="Bill Split" component={BillSplitScreen} />
       <Tab.Screen name="Profile" component={AppNavigator} />
     </Tab.Navigator>
