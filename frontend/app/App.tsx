efe58ab42fdc1e83f4083d202cc8d76b60c3609c
Alice Dang <dthuhang214@gmail.com>
2025-04-30 22:35:58 -0400

    Remove duplicate on App.tsx

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import React, { useState } from "react";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import AuthNavigator from "./navigation/AuthNavigator";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
-import GroupSavingNavigator from "./navigation/GroupSavingNavigator"; // Import the correct navigator
+
 import { UserProvider } from "./screens/Auth/UserContext"; // Import the UserProvider
 const RootStack = createStackNavigator();
 
@@ -26,9 +26,7 @@ export default function App() {
       <RootStack.Navigator>
         <RootStack.Screen name="Main" component={RootNavigator} options={{ headerShown: false }} />
       </RootStack.Navigator>
-      <RootStack.Navigator>
-        <RootStack.Screen name="Main" component={RootNavigator} options={{ headerShown: false }} />
-      </RootStack.Navigator>
+
     </NavigationContainer>
     </UserProvider>
   );
